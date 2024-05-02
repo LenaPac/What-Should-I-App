d0c5dcdc90002dfda98762fca9fb3b27f5359b67
LenaPac <143618741+LenaPac@users.noreply.github.com>
2024-05-02 14:29:31 +0000

    New

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // Declare your arrays here
 let longAnimes = [
-    "https://m.media-amazon.com/images/M/MV5BOGZmYjdjN2UtNjAwZi00YmEyLWFhNTEtNjM1OTc5ODg0MGEyXkEyXkFqcGdeQXVyMTA1NjQyNjkw._V1_.jpg",
-"https://upload.wikimedia.org/wikipedia/en/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg",
-"https://m.media-amazon.com/images/M/MV5BNGM0YTk3MWEtN2JlZC00ZmZmLWIwMDktZTMxZGE5Zjc2MGExXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg",
+"Myhero.jpg",
+"OnePiece.jpg",
+"HunterXHunter.jpg",
 ];
 
 let midAnimes = [
-"https://m.media-amazon.com/images/M/MV5BNTYyYmFkMGMtN2YwNi00MDVjLWFmNWUtMTRjYWNhZmY4NGI0XkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_FMjpg_UX1000_.jpg",
-"https://m.media-amazon.com/images/M/MV5BZWNiY2IwMGUtYTk1NC00NmE5LWE4NTItYmQyNDJmMGU2MDMwXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg",
-"https://m.media-amazon.com/images/M/MV5BNTdiNTJmMzktZmE2MC00OWUxLWE1MjktMTdhN2Q1OWNlODY1L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
-];
+"FruitsBasket.jpg",
+"Horimiya.jpg",
+"SoulEater.jpg",];
 
 let shortAnimes = [
-"https://m.media-amazon.com/images/M/MV5BMzkyYWY3NTEtYzBkNC00Zjc3LThmY2UtZWU3MTRkMTgwODU5XkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_FMjpg_UX1000_.jpg",
-"https://m.media-amazon.com/images/M/MV5BMmIxNWFjYzgtM2NkMy00NTdkLWE4ZTEtMmY3NzU2MjA5MjAxXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
-"https://m.media-amazon.com/images/M/MV5BMjdiMTAzZjItMWQ3MS00Y2FiLWFkOTYtZmQ1MTE1YzJjOGRhXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg",
+"Mydressup.jpg",
+"Asign.jpg",
+"SK8.jpg",
 ];
 
 let list;
@@ -34,12 +33,14 @@ if (span === "Long")
 {
    imageContainer.innerHTML="";
         list = longAnimes;
+        imageContainer.innerHTML="";
         console.log(span);
 }
 else if (span==="Mid")
 
 {imageContainer.innerHTML="";
     list=midAnimes; 
+    imageContainer.innerHTML="";
 
 
 }
@@ -47,7 +48,7 @@ else if(span==="Short")
 
 {imageContainer.innerHTML="";
     list=shortAnimes;
-
+    imageContainer.innerHTML="";
 }
 
 
